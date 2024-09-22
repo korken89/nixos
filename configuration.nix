4747096ff03e935ee052562f8124fd03a0ba2c72
Emil Fresk <emil.fresk@gmail.com>
2024-09-22 13:14:54 +0200

    32 current  2024-09-22 13:14:52  24.05.4997.086b448a5d54  6.6.51                          *

--- configuration.nix
+++ configuration.nix
@@ -232,4 +232,8 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 }
