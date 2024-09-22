09617f28cd564859dd8f0263679b84484ccd5bb8
Emil Fresk <emil.fresk@gmail.com>
2024-09-22 16:06:31 +0200

    37 current  2024-09-22 16:06:29  24.05.5197.944b2aea7f0a  6.11.0                          *

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,9 @@
     ./hardware-configuration.nix
   ];
 
+  # Kernel (latest)
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
