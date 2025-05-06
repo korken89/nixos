7c53aab6d9176b85ff6684a828263ce6b96ada96
Emil Fresk <emil.fresk@gmail.com>
2025-05-06 22:12:31 +0200

    Update nixos

--- flake.nix
+++ flake.nix
@@ -113,7 +113,7 @@
               x1e-nixos-config.nixosModules.x1e
               {
                 networking.hostName = "emifre-yoga-7x-nixos";
-                hardware.deviceTree.name = "qcom/x1e80100-lenovo-yoga-slim7x.dtb";
+                hardware.lenovo-yoga-slim7x.enable = true;
 
                 nixpkgs.hostPlatform.system = "aarch64-linux";
 
