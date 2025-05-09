fccb2a7d56eebc83a7c4319d30a2291d004395e6
Emil Fresk <emil.fresk@gmail.com>
2025-05-09 21:52:25 +0200

    Basic tmux with sessionizer

--- modules/common.nix
+++ modules/common.nix
@@ -6,6 +6,37 @@
   lib,
   ...
 }:
+let
+  tmux-sessionizer = pkgs.writeShellScriptBin "tmux-sessionizer" ''
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    selected=$(fd --type d --hidden '^\.git$' ~/Git ~/Work -x dirname | sed "s|^$HOME|~|" | sort | fzf)
+fi
+
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected
+    exit 0
+fi
+
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
+fi
+
+if [[ -z $TMUX ]]; then
+    tmux attach -t $selected_name
+else
+    tmux switch-client -t $selected_name
+fi
+  '';
+in
 {
 
   imports = [
@@ -48,6 +79,7 @@
     ripgrep
     sd
     syncthing
+    tmux-sessionizer
     wget
     wireshark
 
@@ -168,6 +200,7 @@
 
       if status --is-interactive
           keychain --eval --quiet -Q id_ed25519 | source
+          bind \cf "tmux-sessionizer"
       end
 
       begin
