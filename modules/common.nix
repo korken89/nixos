c96b8057bc2ac11403cb56784c5e6e1b20b2dd9a
Emil Fresk <emil.fresk@gmail.com>
2025-05-12 07:56:39 +0200

    fmt

--- modules/common.nix
+++ modules/common.nix
@@ -8,33 +8,33 @@
 }:
 let
   tmux-sessionizer = pkgs.writeShellScriptBin "tmux-sessionizer" ''
-if [[ $# -eq 1 ]]; then
-    selected=$1
-else
-    selected=$(fd --type d --hidden '^\.git$' ~/Git ~/Work -x dirname | sed "s|^$HOME|~|" | sort | fzf | sed "s|^~|$HOME|")
-fi
+    if [[ $# -eq 1 ]]; then
+        selected=$1
+    else
+        selected=$(fd --type d --hidden '^\.git$' ~/Git ~/Work -x dirname | sed "s|^$HOME|~|" | sort | fzf | sed "s|^~|$HOME|")
+    fi
 
-if [[ -z $selected ]]; then
-    exit 0
-fi
+    if [[ -z $selected ]]; then
+        exit 0
+    fi
 
-selected_name=$(basename "$selected" | tr . _)
-tmux_running=$(pgrep tmux)
+    selected_name=$(basename "$selected" | tr . _)
+    tmux_running=$(pgrep tmux)
 
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c $selected
-    exit 0
-fi
+    if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+        tmux new-session -s $selected_name -c $selected
+        exit 0
+    fi
 
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected
-fi
+    if ! tmux has-session -t=$selected_name 2> /dev/null; then
+        tmux new-session -ds $selected_name -c $selected
+    fi
 
-if [[ -z $TMUX ]]; then
-    tmux attach -t $selected_name
-else
-    tmux switch-client -t $selected_name
-fi
+    if [[ -z $TMUX ]]; then
+        tmux attach -t $selected_name
+    else
+        tmux switch-client -t $selected_name
+    fi
   '';
 in
 {
