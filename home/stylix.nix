bf8d9170ebb8a3078138b476f259ee12b7402d81
Emil Fresk <emil.fresk@gmail.com>
2025-05-26 20:10:40 +0200

    Disable wallpaper

--- home/stylix.nix
+++ home/stylix.nix
@@ -18,10 +18,10 @@
       name = "Adwaita";
       size = 24;
     };
-    image = pkgs.fetchurl {
-      url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-      sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-    };
+    # image = pkgs.fetchurl {
+    #   url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+    #   sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+    # };
     base16Scheme = "${pkgs.base16-schemes}/share/themes/default-dark.yaml";
     fonts = {
       monospace = {
