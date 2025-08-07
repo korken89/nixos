7f8a509fe13570fadf5613dfe8566580ae6b888e
Emil Fresk <emil.fresk@gmail.com>
2025-08-07 22:06:26 +0200

    Add automatic optimization/gc

--- nixos/default.nix
+++ nixos/default.nix
@@ -286,8 +286,17 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+    optimise.automatic = true;
+
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
 }
