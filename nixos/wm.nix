a942cbf6be414eb467cc3ee69a89a68a0543e16b
Emil Fresk <emil.fresk@gmail.com>
2025-09-24 17:56:37 +0200

    Move niri env variables to nixos

--- nixos/wm.nix
+++ nixos/wm.nix
@@ -10,7 +10,17 @@
   programs.niri.enable = true;
   programs.niri.package = pkgs.niri;
 
-  # Nix packages configure Chrome and Electron apps to run in native Wayland
-  # mode if this environment variable is set.
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables = {
+    # Nix packages configure Chrome and Electron apps to run in native Wayland
+    # mode if this environment variable is set.
+    NIXOS_OZONE_WL = "1";
+
+    # Fixes some Java apps, e.g. file picker not showing (stm32cubemx)
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+
+    # If xwayland-satellite.service is running it defines an Xwayland display
+    # with this identifier.
+    DISPLAY = ":0";
+  };
+
 }
