7e1a374da33c5bece3cf370f896423f009c6e134
Emil Fresk <emil.fresk@gmail.com>
2025-11-23 10:28:07 +0100

    Enable openems again

--- home/default.nix
+++ home/default.nix
@@ -13,7 +13,7 @@ in
     ./gtk.nix
     ./helix.nix
     ./notes.nix
-    # ./openems.nix
+    ./openems.nix
     ./packages/analysis.nix
     ./packages/applications.nix
     ./packages/cli-tools.nix
