dc138cceb0b1acf7028e30e336b0cbe7ccae088d
Emil Fresk <emil.fresk@gmail.com>
2025-12-01 22:22:59 +0100

    make ctrl-d not close terminals

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,12 +1,13 @@
 # Auto-start Niri on TTY1
 if test -z "$WAYLAND_DISPLAY" && test "$XDG_VTNR" -eq 1
-  exec @niri@/bin/niri-session
+    exec @niri@/bin/niri-session
 end
 
 # Interactive shell setup
 if status --is-interactive
     @direnv@/bin/direnv hook fish | source
     @keychain@/bin/keychain --eval --quiet -Q id_ed25519 | source
+    bind \cd forward-char
 end
 
 # Keychain integration
@@ -19,26 +20,26 @@ end
 
 # ls replacement with eza
 function ls -d 'eza instead of ls when output is a terminal'
-  if isatty 1
-    if type --quiet @eza@/bin/eza
-      @eza@/bin/eza --group-directories-first --git --icons $argv
+    if isatty 1
+        if type --quiet @eza@/bin/eza
+            @eza@/bin/eza --group-directories-first --git --icons $argv
+        else
+            command ls --color=auto $argv
+        end
     else
-      command ls --color=auto $argv
+        command ls $argv
     end
-  else
-    command ls $argv
-  end
 end
 
 # ls aliases
 function ll -d 'alias ls -l'
-  ls -l $argv
+    ls -l $argv
 end
 
 function lt
-  ls -l --tree $argv
+    ls -l --tree $argv
 end
 
 function la -d 'alias ls -la'
-  ls -la $argv
+    ls -la $argv
 end
