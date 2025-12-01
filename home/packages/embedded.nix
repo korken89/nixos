d245e2bd94729c0bb41458818f6afe69f3597c58
Emil Fresk <emil.fresk@gmail.com>
2025-12-01 22:23:47 +0100

    nix fmt

--- home/packages/embedded.nix
+++ home/packages/embedded.nix
@@ -1,27 +1,35 @@
-{ pkgs, lib, system, ... }:
 {
-  home.packages = with pkgs; [
-    # Embedded/Hardware Development
-    probe-rs-tools
-    qemu
-    sdcc
-    tio
-    usbtop
-    udev
+  pkgs,
+  lib,
+  system,
+  ...
+}:
+{
+  home.packages =
+    with pkgs;
+    [
+      # Embedded/Hardware Development
+      probe-rs-tools
+      qemu
+      sdcc
+      tio
+      usbtop
+      udev
 
-    # Hardware analysis
-    pulseview
-    sigrok-cli
-    sigrok-firmware-fx2lafw
+      # Hardware analysis
+      pulseview
+      sigrok-cli
+      sigrok-firmware-fx2lafw
 
-    # Microcontroller tools
-    stm32cubemx
+      # Microcontroller tools
+      stm32cubemx
 
-    # EDA tools
-    freecad
-    kicad
-  ] ++ lib.optionals (system == "x86_64-linux") [
-    # x86_64 only
-    saleae-logic-2
-  ];
+      # EDA tools
+      freecad
+      kicad
+    ]
+    ++ lib.optionals (system == "x86_64-linux") [
+      # x86_64 only
+      saleae-logic-2
+    ];
 }
--- nixos/shell.nix
+++ nixos/shell.nix
@@ -3,10 +3,11 @@
   # Fish shell
   programs.fish = {
     enable = true;
-    interactiveShellInit = builtins.replaceStrings
-      [ "@niri@" "@direnv@" "@keychain@" "@eza@" ]
-      [ "${pkgs.niri}" "${pkgs.direnv}" "${pkgs.keychain}" "${pkgs.eza}" ]
-      (builtins.readFile ../dotfiles/fish/config.fish);
+    interactiveShellInit =
+      builtins.replaceStrings
+        [ "@niri@" "@direnv@" "@keychain@" "@eza@" ]
+        [ "${pkgs.niri}" "${pkgs.direnv}" "${pkgs.keychain}" "${pkgs.eza}" ]
+        (builtins.readFile ../dotfiles/fish/config.fish);
   };
   documentation.man.generateCaches = false; # fish causes super slow builds if this is on
   programs.starship.enable = true;
