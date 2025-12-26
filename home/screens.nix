7bd90341d9d04d3520ea086fe1f2c01abdb0bea5
Emil Fresk <emil.fresk@gmail.com>
2025-12-26 16:16:03 +0100

    Get kanshi to control screen scaling etc

--- home/screens.nix
+++ home/screens.nix
@@ -13,7 +13,7 @@
         # Work screens
         {
           profile = {
-            name = "work";
+            name = "work_workstation";
             outputs = [
               # {
               #   criteria = "Samsung Electric Company C34J79x H4ZRA01366";
@@ -24,13 +24,27 @@
                 criteria = "Lenovo Group Limited L32p-30 U5128TW7";
                 mode = "3840x2160@59.997";
                 position = "0,0";
-                scale = 1.25;
+                scale = 1.0;
               }
               {
                 criteria = "Dell Inc. DELL U2518D 3C4YP7BK268L";
                 mode = "2560x1440@59.951";
                 position = "3840,-200";
                 transform = "270";
+                scale = 1.0;
+              }
+            ];
+          };
+        }
+
+        # Thinkpad E14 laptop
+        {
+          profile = {
+            name = "thinkpad_e14";
+            outputs = [
+              {
+                criteria = "AU Optronics 0x403D Unknown";
+                scale = 1.0;
               }
             ];
           };
@@ -38,50 +52,59 @@
 
         # Yoga 7x laptop
         {
-          output = {
-            criteria = "Samsung Display Corp. 0x4189 Unknown";
-            scale = 1.5;
-            alias = "yoga_laptop";
+          profile = {
+            name = "yoga_7x";
+            outputs = [
+              {
+                criteria = "Samsung Display Corp. 0x4189 Unknown";
+                scale = 1.5;
+              }
+            ];
           };
         }
 
         # 42" LG OLED TV screen
         {
-          output = {
-            criteria = "LG Electronics LG TV SSCR2 0x01010101";
-            mode = "3840x2160@119.880";
-            position = "0,0";
-            alias = "home_lg_tv";
+          profile = {
+            name = "home_workstation";
+            outputs = [
+              {
+                criteria = "LG Electronics LG TV SSCR2 0x01010101";
+                mode = "3840x2160@119.880";
+                position = "0,0";
+                # alias = "home_lg_tv";
+              }
+            ];
           };
         }
 
-        # Yoga 7x laptop docking
-        {
-          profile = {
-            name = "undocked";
-            outputs = [
-              {
-                criteria = "eDP-1";
-                status = "enable";
-              }
-            ];
-          };
-        }
-        {
-          profile = {
-            name = "home_large_docked";
-            outputs = [
-              {
-                criteria = "$yoga_laptop";
-                status = "disable";
-              }
-              {
-                criteria = "$home_lg_tv";
-                status = "enable";
-              }
-            ];
-          };
-        }
+        # # yoga 7x laptop docking
+        # {
+        #   profile = {
+        #     name = "undocked";
+        #     outputs = [
+        #       {
+        #         criteria = "edp-1";
+        #         status = "enable";
+        #       }
+        #     ];
+        #   };
+        # }
+        # {
+        #   profile = {
+        #     name = "home_large_docked";
+        #     outputs = [
+        #       {
+        #         criteria = "$yoga_laptop";
+        #         status = "disable";
+        #       }
+        #       {
+        #         criteria = "$home_lg_tv";
+        #         status = "enable";
+        #       }
+        #     ];
+        #   };
+        # }
       ];
     };
   };
