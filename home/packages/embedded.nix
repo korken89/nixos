604465b60b2f7a81da62ac9747c891cb3270a3ab
Emil Fresk <emil.fresk@gmail.com>
2026-03-16 20:39:31 +0100

    Add freecad-zink (working on nvidia)

--- home/packages/embedded.nix
+++ home/packages/embedded.nix
@@ -4,6 +4,16 @@
   system,
   ...
 }:
+let
+  # FreeCAD with Zink (OpenGL-over-Vulkan) workaround for NVIDIA on Wayland
+  # https://github.com/NixOS/nixpkgs/issues/468456
+  freecad-zink = pkgs.runCommand "freecad-zink" { nativeBuildInputs = [ pkgs.makeWrapper ]; } ''
+    mkdir -p $out/bin
+    makeWrapper ${pkgs.freecad-wayland}/bin/FreeCAD $out/bin/freecad-zink \
+      --set MESA_LOADER_DRIVER_OVERRIDE zink \
+      --set __EGL_VENDOR_LIBRARY_FILENAMES ${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json
+  '';
+in
 {
   home.packages =
     with pkgs;
@@ -26,6 +36,7 @@
 
       # EDA tools
       freecad
+      freecad-zink
       (kicad.override { compressStep = false; })
       librepcb
     ]
