3ec4d0128cdd57c89e5bd5b4bde2ea88f7bc2298
Emil Fresk <emil.fresk@gmail.com>
2026-06-13 09:59:12 +0200

    Update

--- overlays/claude-code.nix
+++ overlays/claude-code.nix
@@ -1,23 +1,23 @@
 final: prev:
 let
-  version = "2.1.170";
+  version = "2.1.177";
   baseUrl = "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases";
   checksums = {
     "aarch64-darwin" = {
       platformKey = "darwin-arm64";
-      sha256 = "e903646d8b7a31882a80ecd27569a27d8ac57b3708745f349709632c84117fdf";
+      sha256 = "eb0730351be2f02b482b1855870f5877489085aac86b0c4c1db4e458d9e40ed9";
     };
     "x86_64-darwin" = {
       platformKey = "darwin-x64";
-      sha256 = "914f23a70bbed5d9ae567e3e04b86206ed9971b371bc9baca3f79c8885bfddb4";
+      sha256 = "fcdc6c6679d4e1e3a0a3812f24e8b08ab73156732072c2ca5ee6248bee8313bd";
     };
     "aarch64-linux" = {
       platformKey = "linux-arm64";
-      sha256 = "1bb9d032440a75532f7dd4cafbc687f220aaf16c63eba17e192dfbec2f04bd25";
+      sha256 = "baf3926dc166215772f959e367da9784ff4c75157aaafe4524fdc79ebff984b1";
     };
     "x86_64-linux" = {
       platformKey = "linux-x64";
-      sha256 = "849e007277a0442ab27570d3e3d6d43787507946590e8dd1947e5a39b7081f9e";
+      sha256 = "ff41753634b20c869ef6a32a20863521b33d4186ac0d6a49379ab48a48395ee7";
     };
   };
   entry = checksums.${prev.stdenv.hostPlatform.system};
